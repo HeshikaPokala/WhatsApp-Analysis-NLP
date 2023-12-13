a9a04a31ba4c8eed9153f505f992a1ef9ee11b83
VasundharaReddy33 <137511587+VasundharaReddy33@users.noreply.github.com>
2023-12-14 00:28:16 +0530

    helper.py

--- helper.py
+++ helper.py
@@ -10,23 +10,31 @@ def fetch_stats(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
+    # Convert the 'message' column to strings to handle NaN values
+    df['message'] = df['message'].astype(str)
+
     # fetch the number of messages
     num_messages = df.shape[0]
+
     # fetch the total number of words
     words = []
     for message in df['message']:
-        if isinstance(message, str):  # Check if the message is a string
+        # Handle NaN values by checking if the message is not NaN
+        if not pd.isna(message):
             words.extend(message.split())
 
     # fetch number of media messages
     num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
     # fetch number of links shared
     links = []
     for message in df['message']:
-        if isinstance(message, str):  # Check if the message is a string
+        # Handle NaN values by checking if the message is not NaN
+        if not pd.isna(message):
             links.extend(extract.find_urls(message))
 
     return num_messages, len(words), num_media_messages, len(links)
+
 
 
 def most_busy_users(df):
@@ -39,8 +47,10 @@ def most_busy_users(df):
 def create_wordcloud(selected_user, df):
     f = open(r"C:\Users\kumar_lf3uub3\Desktop\CN\Whatsapp Analyser\stop_hinglish.txt", 'r')
     stop_words = f.read()
+
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     temp = df[df['user'] != 'group_notification']
     temp = temp[temp['message'] != '<Media omitted>\n']
 
@@ -50,6 +60,7 @@ def create_wordcloud(selected_user, df):
             if word not in stop_words:
                 y.append(word)
         return " ".join(y)
+
     wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
     temp['message'] = temp['message'].apply(remove_stop_words)
     df_wc = wc.generate(temp['message'].str.cat(sep=" "))
@@ -59,10 +70,13 @@ def create_wordcloud(selected_user, df):
 def most_common_words(selected_user, df):
     f = open(r"C:\Users\kumar_lf3uub3\Desktop\CN\Whatsapp Analyser\stop_hinglish.txt", 'r')
     stop_words = f.read()
+
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     temp = df[df['user'] != 'group_notification']
     temp = temp[temp['message'] != '<Media omitted>\n']
+
     words = []
 
     for message in temp['message']:
@@ -77,36 +91,45 @@ def most_common_words(selected_user, df):
 def monthly_timeline(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+
     time = []
     for i in range(timeline.shape[0]):
         time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
     timeline['time'] = time
+
     return timeline
 
 
 def daily_timeline(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+
     return daily_timeline
 
 
 def week_activity_map(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     return df['day_name'].value_counts()
 
 
 def month_activity_map(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     return df['month'].value_counts()
 
 
 def activity_heatmap(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
+
     user_heatmap = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
 
     return user_heatmap
